f9baef58daf27cf1ddd560a6275710475876b231
ggcy <3246907890@qq.com>
2025-02-11 20:09:37 +0800

    [案例]-拆分:DialogDemoCtl

---
+++ Samples/Extend_Controls/DialogDemoCtl/ViewModelLocator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using HandyControlDemo.ViewModel;
+
+namespace DialogDemoCtl.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<DialogDemoViewModel>();
+            SimpleIoc.Default.Register<InteractiveDialogViewModel>();
+        }
+
+        public static ViewModelLocator Instance = new Lazy<ViewModelLocator>(() =>
+    Application.Current.TryFindResource("Locator") as ViewModelLocator).Value;
+
+        public DialogDemoViewModel DialogDemo => SimpleIoc.Default.GetInstance<DialogDemoViewModel>();
+
+        public InteractiveDialogViewModel InteractiveDialog => SimpleIoc.Default.GetInstance<InteractiveDialogViewModel>();
+    }
+}
