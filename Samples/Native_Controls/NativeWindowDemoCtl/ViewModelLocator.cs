ec57e74d650f425f477b6734afbd18d36ac41542
ggcy <3246907890@qq.com>
2025-02-13 23:41:58 +0800

    [案例]-拆分:NativeWindowDemoCtl

---
+++ Samples/Native_Controls/NativeWindowDemoCtl/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using HandyControlDemo.ViewModel;
+
+namespace NativeWindowDemoCtl.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<WindowDemoViewModel>();
+        }
+
+        public static ViewModelLocator Instance = new Lazy<ViewModelLocator>(() =>
+    Application.Current.TryFindResource("Locator") as ViewModelLocator).Value;
+
+
+        public WindowDemoViewModel WindowDemo => SimpleIoc.Default.GetInstance<WindowDemoViewModel>();
+    }
+}
