6a98a31298cbd7a56baf00acbbd1bd51dea12ea5
ggcy <3246907890@qq.com>
2025-02-13 13:02:52 +0800

    [案例]-拆分:ListViewDemoCtl

---
+++ Samples/Native_Controls/ListViewDemoCtl/ViewModelLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using HandyControlDemo.Service;
+using HandyControlDemo.ViewModel;
+
+namespace ListViewDemoCtl.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<DataService>();
+            var dataService = SimpleIoc.Default.GetInstance<DataService>();
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public static ViewModelLocator Instance = new Lazy<ViewModelLocator>(() =>
+    Application.Current.TryFindResource("Locator") as ViewModelLocator).Value;
+
+
+        public MainViewModel Main => SimpleIoc.Default.GetInstance<MainViewModel>();
+    }
+}
