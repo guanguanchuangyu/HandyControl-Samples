92e16d075886d1d8dd6ce67a3560345aeb00f917
ggcy <3246907890@qq.com>
2025-02-13 22:48:32 +0800

    [案例]-拆分:ToolBarDemoCtl

---
+++ Samples/Native_Controls/ToolBarDemoCtl/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Windows;
+using HandyControlDemo.Properties.Langs;
+
+namespace ToolBarDemoCtl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LangProvider.Culture = new CultureInfo("zh-cn");
+            base.OnStartup(e);
+        }
+    }
+
+}
