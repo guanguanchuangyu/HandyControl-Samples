05a89753f8bacf0bc4051e0469521b9a48ff502f
ggcy <3246907890@qq.com>
2025-03-04 23:06:22 +0800

    [案例]-拆分:ComboBoxDemoCtl

---
+++ Samples/Extend_Controls/ComboBoxDemoCtl/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using HandyControlDemo.ViewModel;
+
+namespace ComboBoxDemoCtl.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<InputElementDemoViewModel>();
+        }
+
+        public static ViewModelLocator Instance = new Lazy<ViewModelLocator>(() =>
+    Application.Current.TryFindResource("Locator") as ViewModelLocator).Value;
+
+
+        public InputElementDemoViewModel InputElementDemo => SimpleIoc.Default.GetInstance<InputElementDemoViewModel>();
+    }
+}
