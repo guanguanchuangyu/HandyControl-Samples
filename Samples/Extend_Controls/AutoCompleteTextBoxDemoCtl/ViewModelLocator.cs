5f0bb1f21f94ba33b6e1ef2facb2871026a7ad49
ggcy <3246907890@qq.com>
2025-03-05 00:25:22 +0800

    [案例]-拆分:AutoCompleteTextBoxDemoCtl

---
+++ Samples/Extend_Controls/AutoCompleteTextBoxDemoCtl/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using HandyControlDemo.Service;
+using HandyControlDemo.ViewModel;
+
+namespace AutoCompleteTextBoxDemoCtl.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<DataService>();
+            SimpleIoc.Default.Register<InputElementDemoViewModel>();
+            SimpleIoc.Default.Register<AutoCompleteTextBoxDemoViewModel>();
+        }
+
+        public static ViewModelLocator Instance = new Lazy<ViewModelLocator>(() =>
+    Application.Current.TryFindResource("Locator") as ViewModelLocator).Value;
+
+
+        public InputElementDemoViewModel InputElementDemo => SimpleIoc.Default.GetInstance<InputElementDemoViewModel>();
+        public AutoCompleteTextBoxDemoViewModel AutoCompleteTextBoxDemo => SimpleIoc.Default.GetInstance<AutoCompleteTextBoxDemoViewModel>();
+    }
+}
